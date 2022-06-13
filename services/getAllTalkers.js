076d3f20dcdc471af406be5287bf0d5b0d7209e5
assisMeneghetti <assismfabri@hotmail.com>
2022-06-13 17:17:26 -0300

    feat: create getAllTalkers middleware

---
+++ services/getAllTalkers.js
@@ -0,0 +1,11 @@
+const { readFile } = require('fs/promises');
+
+module.exports = async (_req, res, next) => {
+  try {
+    const talkers = await readFile('./talker.json', 'utf-8');
+    const parsedTalkers = JSON.parse(talkers);
+    return res.status(200).json(parsedTalkers);
+  } catch (e) {
+    return next(e);
+  }
+};

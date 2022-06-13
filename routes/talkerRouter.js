48b065677326dc40dff8e1a8734dcb31b69349e7
assisMeneghetti <assismfabri@hotmail.com>
2022-06-13 17:46:06 -0300

    feat: create talker/:id endpoint

--- routes/talkerRouter.js
+++ routes/talkerRouter.js
@@ -4,5 +4,6 @@ const talkerRouter = express.Router();
 const services = require('../services');
 
 talkerRouter.get('/talker', services.getAllTalkers);
+talkerRouter.get('/talker/:id', services.getTalkerById);
 
 module.exports = { talkerRouter };

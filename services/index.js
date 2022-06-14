5029b7e28d75cf0946664dd2609d7cf5e19afcf5
assisMeneghetti <assismfabri@hotmail.com>
2022-06-14 10:35:32 -0300

    feat: export createTalker by index

--- services/index.js
+++ services/index.js
@@ -1,9 +1,11 @@
 const getAllTalkers = require('./getAllTalkers');
 const getTalkerById = require('./getTalkerById');
 const login = require('./login');
+const createTalker = require('./createTalker');
 
 module.exports = { 
   getAllTalkers,
   getTalkerById,
   login,
+  createTalker,
 };

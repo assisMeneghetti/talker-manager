429aba2fc46e788ee1b68439bef3f446b3082e8f
assisMeneghetti <assismfabri@hotmail.com>
2022-06-14 10:53:09 -0300

    feat: create router put /talker/:id

--- routes/talkerRouter.js
+++ routes/talkerRouter.js
@@ -8,5 +8,6 @@ talkerRouter.get('/talker', services.getAllTalkers);
 talkerRouter.get('/talker/:id', services.getTalkerById);
 talkerRouter.post('/login', services.login);
 talkerRouter.post('/talker', rescue(services.createTalker));
+talkerRouter.put('/talker/:id', rescue(services.editTalker));
 
 module.exports = { talkerRouter };

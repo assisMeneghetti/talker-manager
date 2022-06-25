e241850825324bacf256e9c815b46e4d29b2ce44
assisMeneghetti <assismfabri@hotmail.com>
2022-06-25 15:08:33 -0300

    feat: create readContentFile function

---
+++ helpers/readWriteFile.js
@@ -0,0 +1,33 @@
+// src: https://github.dev/tryber/sd-019-c-live-lectures/tree/lecture/22.6-exercicio4
+const fs = require('fs').promises;
+
+const readContentFile = async (path) => {
+  try {
+    const content = await fs.readFile(path, 'utf8');
+    return JSON.parse(content);
+  } catch (error) {
+    console.log(error.message);
+    return null;
+  }
+};
+
+const writeContentFile = async (path, content, op) => {
+  try {
+    const arrContent = await readContentFile(path) || [];
+    if (op === 'delete') {
+      await fs.writeFile(path, JSON.stringify(content, null, 2));
+      return content;
+    } 
+      arrContent.push(content); 
+      await fs.writeFile(path, JSON.stringify(arrContent, null, 2));
+      return content;
+  } catch (error) {
+    console.log(error.message);
+    return null;
+  }
+};
+
+module.exports = {
+  readContentFile,
+  writeContentFile,
+};

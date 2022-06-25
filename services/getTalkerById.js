127e5fcaffb158b904c6967964c4f50cdfaff811
assisMeneghetti <assismfabri@hotmail.com>
2022-06-25 15:20:00 -0300

    refactor: add readContentFile to getTalkerById services

--- services/getTalkerById.js
+++ services/getTalkerById.js
@@ -1,10 +1,9 @@
-const { readFile } = require('fs/promises');
+const { readContentFile } = require('../helpers');
 
 module.exports = async (req, res, next) => {
   try {
     const { id } = req.params;
-    const talkers = await readFile('./talker.json', 'utf-8');
-    const parsedTalkers = JSON.parse(talkers);
+    const parsedTalkers = await readContentFile('./talker.json');
     const talker = parsedTalkers.find((t) => t.id === +id);
     if (!talker) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
     return res.status(200).json(talker);

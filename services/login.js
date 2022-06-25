e8a56bb1e9b0437725bf21fa829269ea26468b12
assisMeneghetti <assismfabri@hotmail.com>
2022-06-25 15:25:02 -0300

    chore: remove comments

--- services/login.js
+++ services/login.js
@@ -3,16 +3,12 @@ const loginSchema = require('../schemas/loginSchema');
 
 module.exports = async (req, res, next) => {
   try {
-    // const { email, password } = req.body;
     const { error } = loginSchema.validate(req.body);
 
     if (error) {
       const [code, message] = error.message.split('|');
       return res.status(code).json({ message });
     }
-    // criar um middleware de autenticação
-    // encriptar a senha e salvar 
-    // salvar o email e a senha em um arquivo para validação
     const token = crypto.randomBytes(8).toString('hex');
     return res.status(200).json({ token });
   } catch (e) {
